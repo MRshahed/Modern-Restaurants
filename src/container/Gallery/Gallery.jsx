ad00363d296e95dbd9e21703d76a9825f16aa2ac
MRshahed <shahedhossensaad@gmail.com>
2022-10-17 04:53:02 +0600

    updated

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
-
+import React, { useRef, useState } from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+import {images} from "../../constants";
+import {SubHeading} from "../../components";
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
+const Images = [images.gallery01, images.gallery02, images.gallery03, images.gallery04];
+const Gallery = () => 
+{
+ const scrollRef = useRef(null);
+ const scroll = (direction)=>{
+   const {current} = scrollRef;
+
+     if(direction === "left"){
+      current.scrollRef -= 300;
+     }
+     else{
+      current.scrollRef += 300;
+     }
+ }
+return (
+  <div className='app_gallery flex__center' id='gallery'>
+  <div className='app_gallery-content'>
+    <SubHeading title="Instagram"/>
+    <h1 className='headtext__cormorant'>Photo Gallery</h1>
+    <p className='p__opensans' style={{color:"#AAA", marginTop:"2rem"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+    <button className='custom__button' type="button">View More</button>
+  </div>
   
+  <div className='app_gallery-images'>
+    <div className='app_gallery-images_container' ref={scrollRef}>
+      {Images.map((img, index)=>{
+        return (
+          <div className='app_gallery-images_card flex__center' key={`gallery_images-${index + 1}`}>
+           <img src={img} alt="gallery"/>
+           <BsInstagram className='gallery_image-icon'/>
+        </div>
+        );
+      })}
+    </div>
+
+    <div className='app_gallery-images_arrow'>
+      <BsArrowLeftShort className='gallery__arrow-icon' onClick={()=> scroll("left")}/>
+      <BsArrowRightShort className='gallery__arrow-icon' onClick={()=> scroll("right")}/>
+    </div>
+  </div>
   </div>
 );
-
+}
 export default Gallery;
