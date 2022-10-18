1a112f59eb0aa721daa4afaeb0ce7ecfc1a33ea9
MRshahed <shahedhossensaad@gmail.com>
2022-10-19 04:10:14 +0600

    Added Footer Section

--- src/components/ScrollTop/ScrollTop.jsx
+++ src/components/ScrollTop/ScrollTop.jsx
@@ -1,44 +1,44 @@
-import React,{useEffect, useState} from "react";
-import images from "../../constants/images";
+// import React,{useEffect, useState} from "react";
+// import images from "../../constants/images";
 
-const ScrollTop = ()=>{
+// const ScrollTop = ()=>{
    
-    const {showButton, setShowbutotn} = useState(false);
-   useEffect(() => {
-     const handleScrollButton = ()=>{
-        window.pageYOffset>300 ? setShowbutotn(true) : setShowbutotn(false);
-     }
-     window.addEventListener("scroll", handleScrollButton);
+//     const {showButton, setShowbutotn} = useState(false);
+//    useEffect(() => {
+//      const handleScrollButton = ()=>{
+//         window.pageYOffset>300 ? setShowbutotn(true) : setShowbutotn(false);
+//      }
+//      window.addEventListener("scroll", handleScrollButton);
    
-     return () => {
-       window.removeEventListener("scroll", handleScrollButton);
-     }
-   }, [])
+//      return () => {
+//        window.removeEventListener("scroll", handleScrollButton);
+//      }
+//    }, [])
    
-   const handleScrollToTopButton = ()=>{
-    window.scrollTo({top: 0, behavior: "smooth"});
-   }
+//    const handleScrollToTopButton = ()=>{
+//     window.scrollTo({top: 0, behavior: "smooth"});
+//    }
 
-    return (
-        <div>
-    {showButton && (
+//     return (
+//         <div>
+//     {showButton && (
          
-        <div className="scroollToTop">
+//         <div className="scroollToTop">
 
-        <button  
-        className="app__scrollButton"
-        onClick={handleScrollToTopButton}
-        >
-        <img src={images.UpArrow} alt="scrool button"/>
+//         <button  
+//         className="app__scrollButton"
+//         onClick={handleScrollToTopButton}
+//         >
+//         <img src={images.UpArrow} alt="scrool button"/>
 
-        </button>
+//         </button>
 
-        </div>
+//         </div>
 
-        )
-    }
-</div>
-    );
-}
+//         )
+//     }
+// </div>
+//     );
+// }
 
-export default ScrollTop;
+// export default ScrollTop;
